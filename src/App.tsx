99ecb5b583ab77af113dd754ed6d709829cdfdd9
genamontana <predator199@yandex.ru>
2022-10-09 14:24:03 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
+function App() {
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My Friends"}/>
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu_1"} collapsed={true}/>
+            <Accordion titleValue={"Menu_2"} collapsed={false}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+
+    console.log('PageTitle rendering')
+    return <h1>{props.title}</h1>
+}
+
+export default App;
