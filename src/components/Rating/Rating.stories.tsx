b90fd6fe131519e4326d52f7f2830c00a9eec2ea
genamontana <predator199@yandex.ru>
2023-01-17 19:37:46 +0300

    10-storybook-completed

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Rating} from './Rating';
+import React, {useState} from 'react';
+import {Rating, RatingValueType} from './Rating';
 
 
 export default {
@@ -7,10 +7,16 @@ export default {
     component: Rating,
 };
 
-export const EmptyRating = ()=><Rating value={0} onClick={x=>x}/>;
-export const Rating1 = ()=><Rating value={1} onClick={x=>x}/>;
-export const Rating2 = ()=><Rating value={2} onClick={x=>x}/>;
-export const Rating3 = ()=><Rating value={3} onClick={x=>x}/>;
-export const Rating4 = ()=><Rating value={4} onClick={x=>x}/>;
-export const Rating5 = ()=><Rating value={5} onClick={x=>x}/>;
+export const EmptyRating = () => <Rating value={0} onClick={x => x}/>;
+export const Rating1 = () => <Rating value={1} onClick={x => x}/>;
+export const Rating2 = () => <Rating value={2} onClick={x => x}/>;
+export const Rating3 = () => <Rating value={3} onClick={x => x}/>;
+export const Rating4 = () => <Rating value={4} onClick={x => x}/>;
+export const Rating5 = () => <Rating value={5} onClick={x => x}/>;
+export const RatingChanging = () => {
+    const [rating, setRating] = useState<RatingValueType>(3);
+    return (
+        <Rating value={rating} onClick={setRating}/>
+    )
+};
 
