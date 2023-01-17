5e17cf0b26eaa15e07ea94bf73a66f3e9e12a953
genamontana <predator199@yandex.ru>
2023-01-17 19:13:22 +0300

    10-storybook-completed

--- src/components/Rating/Rating.stories.tsx
+++ src/components/Rating/Rating.stories.tsx
@@ -12,4 +12,5 @@ export const Rating1 = ()=><Rating value={1} onClick={x=>x}/>;
 export const Rating2 = ()=><Rating value={2} onClick={x=>x}/>;
 export const Rating3 = ()=><Rating value={3} onClick={x=>x}/>;
 export const Rating4 = ()=><Rating value={4} onClick={x=>x}/>;
+export const Rating5 = ()=><Rating value={5} onClick={x=>x}/>;
 
