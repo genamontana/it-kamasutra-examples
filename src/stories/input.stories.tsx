a7b885fe9b48f41384735a50b714f2dcc73fd1bb
genamontana <predator199@yandex.ru>
2023-01-19 18:58:16 +0300

    13-controlled input, checkbox, select-completed

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -56,17 +56,33 @@ export const ControlledInput: Story<any> = (args) => {
 ControlledInput.args = {};
 
 export const ControlledCheckbox: Story<any> = (args) => {
+    const [parentValue, setParentValue] = useState(true)
 
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
     return <>
-        <input type={'checkbox'}/>
+        <input type={'checkbox'}
+               checked={parentValue}
+               onChange={onChange}/>
     </>
 };
 ControlledCheckbox.args = {};
 
 export const ControlledSelect: Story<any> = (args) => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
 
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
     return <>
-        <input type={'checkbox'}/>
+        <select value={parentValue} onChange={onChange}>
+            <option>none</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Moscow</option>
+            <option value={'3'}>Kiev</option>
+
+        </select>
     </>
 };
 ControlledSelect.args = {};
