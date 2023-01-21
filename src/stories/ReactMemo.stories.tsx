cdc2e4955ef4a97a08079564b62954714c8136d0
genamontana <predator199@yandex.ru>
2023-01-22 00:40:24 +0300

    17-React.memo-completed

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -8,15 +8,26 @@ const NewMessagesCounter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
 
-const Users = (props: { users: Array<string> }) => {
-    return <div>{props.users.map((u, i) => <div key={i}>{u}</div>)}</div>
+const UsersSecret = (props: { users: Array<string> }) => {
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
 }
 
+const Users = React.memo(UsersSecret);
+
 export const Example1 = () => {
-    const [counter, setCounter]=useState(0);
-    const [users, setUsers]=useState(['Gena', 'Valera', 'Artem']);
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Gena', 'Valera', 'Artem']);
+
+    const addUser = () => {
+        const newUsers = [...users, ('Vera' + new Date().getTime())]
+        setUsers(newUsers)
+    }
 
     return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={addUser}>add user</button>
         <NewMessagesCounter count={counter}/>
         <Users users={users}/>
     </>
