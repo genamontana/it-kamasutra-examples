675cf31924f6334f899abe875e5aabe33557b5ba
genamontana <predator199@yandex.ru>
2023-03-08 21:31:05 +0300

    21-useEffect-in process (16 min)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -6,20 +6,45 @@ export default {
 
 
 export const SimpleExample = () => {
+    const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1);
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect');
+        document.title = counter.toString()
         //api.getUsers().then('')
         //setInterval
         //indexedDB
         //document.getElementId
         // document.title = 'User';
+    }, [counter])
+
+    // Примеры для того что бы выучить, как работает useEffect
+    // Нарисовали три useEffect и дали в конце разные зависимости
+    // 1-ый случай без зависимостей - перересовывается всегда при перересовки компоненты
+    // 2-ой случай
+    // 3-й случай
+    // ВАЖНО! Что useEffect срабатывает только после того, когда всё отрисуется!!!!
+
+    //--------1-ый случай
+    useEffect(() => {
+        console.log('useEffect every render');
+        document.title = counter.toString()
     })
+    //--------2-ой случай
+    useEffect(() => {
+        console.log('useEffect every render');
+        document.title = counter.toString()
+    },[])
+    //--------3-й случай
+    useEffect(() => {
+        console.log('useEffect every render');
+        document.title = counter.toString()
+    },[counter])
 
     return <>
-        Hello, {counter}
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
 
     </>
 }
