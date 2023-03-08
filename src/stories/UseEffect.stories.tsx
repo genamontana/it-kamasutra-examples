d8d87531e3c8d15cb50045bab9e9239a01fd7f02
genamontana <predator199@yandex.ru>
2023-03-08 21:38:58 +0300

    21-useEffect-completed

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -22,8 +22,8 @@ export const SimpleExample = () => {
     // Примеры для того что бы выучить, как работает useEffect
     // Нарисовали три useEffect и дали в конце разные зависимости
     // 1-ый случай без зависимостей - перересовывается всегда при перересовки компоненты
-    // 2-ой случай
-    // 3-й случай
+    // 2-ой случай с пустой зависимостью - отрисует только 1-ый рендер (аналог componentDidMount)
+    // 3-й случай если в зависимость мы что-то передаём - 1-ый раз когда отрисовка идет и когда конкретный state меняется
     // ВАЖНО! Что useEffect срабатывает только после того, когда всё отрисуется!!!!
 
     //--------1-ый случай
@@ -33,18 +33,19 @@ export const SimpleExample = () => {
     })
     //--------2-ой случай
     useEffect(() => {
-        console.log('useEffect every render');
+        console.log('useEffect only first render (componentDidMount)');
         document.title = counter.toString()
-    },[])
+    }, [])
     //--------3-й случай
     useEffect(() => {
-        console.log('useEffect every render');
+        console.log('useEffect first render and every counter change');
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
 
     return <>
         Hello, {counter} {fake}
-        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setFake(fake + 1)}>fake +</button>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>
 
     </>
 }
