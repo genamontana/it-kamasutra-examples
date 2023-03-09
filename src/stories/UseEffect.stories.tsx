f97aa9230a466690c5b19f94e69016c5e187a6a9
genamontana <predator199@yandex.ru>
2023-03-09 20:26:14 +0300

    22-useEffect-in process

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -49,3 +49,22 @@ export const SimpleExample = () => {
 
     </>
 }
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1);
+
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+        console.log('useEffect');
+        document.title = counter.toString()
+    }, [counter])
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>fake +</button>
+        <button onClick={() => setCounter(counter + 1)}>counter +</button>
+
+    </>
+}
